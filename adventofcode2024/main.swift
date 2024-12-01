6fe44981efe3ff9b01e87fef48e4dc563af5e285
isaksolheim <isak@isak.me>
2024-12-01 16:42:34 +0100

    Solve day 1 part 1

--- adventofcode2024/main.swift
+++ adventofcode2024/main.swift
@@ -1,11 +1,27 @@
-//
-//  main.swift
-//  adventofcode2024
-//
-//  Created by isaks on 01/12/2024.
-//
-
 import Foundation
 
-print("Hello, World!")
+let fileName = "data.txt"
+let currentPath = FileManager.default.currentDirectoryPath
+let filePath = "\(currentPath)/\(fileName)"
 
+do {
+    let content = try String(contentsOfFile: filePath, encoding: .utf8)
+    let lines = content.components(separatedBy: .newlines)
+    let dataPoints = lines.filter { !$0.isEmpty }
+
+    var left = dataPoints.map { Int($0.split(separator: "   ").first!)! }
+    var right = dataPoints.map { Int($0.split(separator: "   ").last!)! }
+
+    left.sort()
+    right.sort()
+
+    var distances: [Int] = []
+
+    for (index, num) in left.enumerated() {
+        distances.append(abs(num - right[index]))
+    }
+
+    print(distances.reduce(0, +))
+} catch {
+    print("error: \(error.localizedDescription)")
+}
