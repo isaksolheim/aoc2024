a456be59163a450ace9e62fc15da0bb37128c883
isaksolheim <isak@isak.me>
2024-12-02 17:00:24 +0100

    Solve day2 part1

--- adventofcode2024/day02/day02.swift
+++ adventofcode2024/day02/day02.swift
@@ -9,7 +9,56 @@ func day02() {
         let content = try String(contentsOfFile: filePath, encoding: .utf8)
         let lines = content.components(separatedBy: .newlines).filter { !$0.isEmpty }
 
-        print(lines)
+        let data = lines.map { $0.split(separator: " ") }
+
+        var safeCount = 0
+
+        for report in data {
+            let formatted = report.map { Int($0)! }
+
+            var prev = -1
+            var forward = true
+
+            for (index, num) in formatted.enumerated() {
+                if prev != -1 {
+                    let diff = abs(num - prev)
+
+                    if diff < 1 || diff > 3 {
+                        break
+                    }
+
+                    if diff == 0 {
+                        break
+                    }
+
+                    if index == formatted.count - 1 {
+                        prev = -1
+                        safeCount += 1
+                    } else {
+                        if forward {
+                            if num > formatted[index + 1] {
+                                break
+                            }
+                        } else {
+                            if num < formatted[index + 1] {
+                                break
+                            }
+                        }
+                    }
+                } else {
+                    if num < formatted[index + 1] {
+                        forward = true
+                    } else {
+                        forward = false
+                    }
+                }
+
+                prev = num
+            }
+        }
+
+        print(safeCount)
+
     } catch {
         print("error: \(error.localizedDescription)")
     }
