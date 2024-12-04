5db632a98492c254af0486fd0e33b57f890d62c6
isaksolheim <isak@isak.me>
2024-12-04 14:21:11 +0100

    Solve day 3 part 2

--- adventofcode2024/day03/day03.swift
+++ adventofcode2024/day03/day03.swift
@@ -9,14 +9,21 @@ func day03() {
         let content = try String(contentsOfFile: filePath, encoding: .utf8)
         let lines = content.components(separatedBy: .newlines).filter { !$0.isEmpty }
 
+        let input = lines.first!
         var total = 0
-        for line in lines {
-            let parts = line.split(separator: ",")
+        var isEnabled = true
 
-            var lastValue = String(parts[1])
-            lastValue.removeLast()
+        let regex = #"mul\([\d]{1,3},[\d]{1,3}\)|do\(\)|don't\(\)"#
+        let instructions = matches(for: regex, in: input)
 
-            total += Int(String(parts[0]).deletingPrefix("mul("))! * Int(lastValue)!
+        for instruction in instructions {
+            if instruction == "do()" {
+                isEnabled = true
+            } else if instruction == "don't()" {
+                isEnabled = false
+            } else if instruction.starts(with: "mul("), isEnabled {
+                total += getNum(from: instruction)
+            }
         }
 
         print(total)
@@ -25,9 +32,32 @@ func day03() {
     }
 }
 
+func getNum(from: String) -> Int {
+    let parts = from.split(separator: ",")
+
+    var lastValue = String(parts[1])
+    lastValue.removeLast()
+
+    return Int(String(parts[0]).deletingPrefix("mul("))! * Int(lastValue)!
+}
+
 extension String {
     func deletingPrefix(_ prefix: String) -> String {
         guard self.hasPrefix(prefix) else { return self }
         return String(self.dropFirst(prefix.count))
+    }
+}
+
+func matches(for regex: String, in text: String) -> [String] {
+    do {
+        let regex = try NSRegularExpression(pattern: regex)
+        let results = regex.matches(in: text,
+                                    range: NSRange(text.startIndex..., in: text))
+        return results.map {
+            String(text[Range($0.range(at: 0), in: text)!])
+        }
+    } catch {
+        print("invalid regex: \(error.localizedDescription)")
+        return []
     }
 }
